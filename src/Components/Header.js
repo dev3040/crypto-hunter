80ae594b2bed305a170641454ce8c8e5f131e883
ajayp <ajayp@itpathsolutions.in>
2022-06-24 12:54:09 +0530

    He

---
+++ src/Components/Header.js
@@ -0,0 +1,66 @@
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import {
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { useNavigate } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  const navigate = useNavigate();
+  const { currency, setCurrency } = CryptoState();
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => navigate("/")}
+              className={classes.title}
+              variant="h5"
+            >
+              Crypto Hunter
+            </Typography>
+            <Select
+              variant="outlined"
+              style={{ width: 100, height: 40, marginLeft: 15 }}
+              value={currency}
+              onChange={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
+
+export default Header;
